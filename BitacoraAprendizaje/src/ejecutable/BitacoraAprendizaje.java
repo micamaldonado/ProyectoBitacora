0d00581c530502031032f8a4b2d53dcfc1e4d1ca
ZileidyAB <zileidy.arguello@upa.edu.py>
2020-10-17 12:44:59 -0300

    Base del Ejecutable Bitacora

--- BitacoraAprendizaje/src/ejecutable/BitacoraAprendizaje.java
+++ BitacoraAprendizaje/src/ejecutable/BitacoraAprendizaje.java
@@ -5,17 +5,72 @@
  */
 package ejecutable;
 
+import clases.GestionBitacora;
+import java.util.Scanner;
+
 /**
  *
  * @author user
  */
 public class BitacoraAprendizaje {
 
+    static Scanner entrada = new Scanner(System.in);
+    private static GestionBitacora unaGestionBitacora = new GestionBitacora();
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int opcion;
+        do {
+            System.out.println();
+            System.out.println("BIENVENIDO A LA GESTION DE BITACORA DE APRENDIZAJE");
+            System.out.println("1. Registrar nuevo alumno");
+            System.out.println("2. Registrar nueva materia");
+            System.out.println("3. Registrar nueva investigación");
+            System.out.println("4. Registrar nuevo conocimiento");
+            System.out.println("5. Registrar nuevo ejercicio");
+            System.out.println("6. Imprimir lista de temas pendientes por alumno");
+            //Asignaciones?
+            System.out.println("7. Salir");
+            System.out.println();
+            System.out.print("Ingrese el número de la opción elegida: ");
+            opcion = entrada.nextInt();
+            entrada.nextLine();
+            System.out.println();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("**************REGISTRAR NUEVO ALUMNO**************");
+                    unaGestionBitacora.registrarAlumno();
+                    break;
+                case 2:
+                    System.out.println("**************REGISTRAR NUEVA MATERIA**************");
+                    unaGestionBitacora.registrarMateria();
+                    break;
+                case 3:
+                    System.out.println("**************REGISTRAR NUEVA INVESTIGACION**************");
+                    //metodo
+                    break;
+                case 4:
+                    System.out.println("**************REGISTRAR NUEVO CONOCIMIENTO**************");
+                    //metodo
+                    break;
+                case 5:
+                    System.out.println("**************REGISTRAR NUEVO EJERCICIO**************");
+                    //metodo
+                    break;
+                case 6:
+                    System.out.println("**************LISTA DE TEMAS PENDIENTE POR ALUMNO**************");
+                    //metodo
+                    break;
+                case 7:
+                    System.out.println("Usted ha decidido salir del sistema");
+                    break;
+                default:
+                    System.out.println("Opción incorrecta. Inténtelo de nuevo");
+            }
+        } while (opcion != 7);
     }
-    
+
 }
